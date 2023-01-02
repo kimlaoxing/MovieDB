6e05146b044af8d5ce20a8f8498e61f6aab354b3
Kevin Maulana <kimlaoxing@gmail.com>
2023-01-02 09:24:16 +0700

    remove unused code

--- Profile/Feature/Profile/Domain/ProfileViewDomain.swift
+++ Profile/Feature/Profile/Domain/ProfileViewDomain.swift
@@ -39,26 +39,3 @@ final class ProfileViewInteractor: ProfileViewUseCaseProtocol {
         }
     }
 }
-//
-//protocol ProfileEditUseCaseProtocol {
-//   func saveEmail(with email: String, forKey: String)
-//   func saveName(with name: String, forKey: String)
-//}
-//
-//final class ProfileEditInteractor: ProfileEditUseCaseProtocol {
-//    private let repository: ProfileEditRepositoryProtocol
-//
-//    required init(
-//        repository: ProfileEditRepositoryProtocol
-//    ) {
-//        self.repository = repository
-//    }
-//
-//    func saveEmail(with email: String, forKey: String) {
-//        self.repository.saveEmail(with: email, forKey: forKey)
-//    }
-//
-//    func saveName(with name: String, forKey: String) {
-//        self.repository.saveName(with: name, forKey: forKey)
-//    }
-//}
